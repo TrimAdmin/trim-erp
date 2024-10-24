e1c6c9c12be81917200bcd12e1e4416d5fd3fd4c
WangYuncheng <w729567588@163.com>
2024-10-24 10:15:31 +0800

    feat(te-web): user dropdown

---
+++ te-web/src/layout/components/header/user-info.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { DropdownMixedOption } from 'naive-ui/es/dropdown/src/interface'
+
+const options: DropdownMixedOption[] = [
+  {
+    label: '个人信息',
+    key: 'UserInfo',
+    icon: () => renderIcon('i-ri:account-circle-line'),
+  },
+  {
+    type: 'divider',
+  },
+  {
+    label: '退出登录',
+    key: 'Logout',
+    icon: () => renderIcon('i-ri:logout-circle-line'),
+  },
+]
+
+function renderIcon(icon: string) {
+  return h('span', {
+    class: icon,
+  })
+}
+</script>
+
+<template>
+  <n-dropdown :options>
+    <div class="flex-c cursor-pointer select-none gap-1">
+      <n-avatar circle size="small">
+        admin
+      </n-avatar>
+      admin
+      <span class="i-ri:arrow-down-s-fill" />
+    </div>
+  </n-dropdown>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- te-web/src/layout/normal/header/index.vue
+++ te-web/src/layout/normal/header/index.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import Breadcrumb from './breadcrumb.vue'
+import UserInfo from '@/layout/components/header/user-info.vue'
+import Breadcrumb from '../../components/header/breadcrumb.vue'
 </script>
 
 <template>
   <n-layout-header
     class="h-header flex-bc px-4 shadow-md"
   >
-    <div class="flex items-center gap-1">
+    <div class="flex items-center gap-2">
       <sider-collapse />
       <Breadcrumb />
     </div>
-    <div class="flex items-center gap-1">
+    <div class="flex items-center gap-2">
       <toggle-dark />
       <switch-locale />
+      <UserInfo />
     </div>
   </n-layout-header>
 </template>
